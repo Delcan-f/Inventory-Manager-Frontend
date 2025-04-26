b9ccf6f623a6010142c027de516f1c5e0b56c861
Delcan-f <109348946+Delcan-f@users.noreply.github.com>
2025-04-26 10:00:41 +1000

    feat: Api health check component

---
+++ tests/ApiHealth.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+
+import ApiHealthWidget from "../src/components/ApiHealthCheck.jsx";
+import { vi } from "vitest";
+
+test("Health check widget sees the API health route", async () => {
+	let renderResult = render(<ApiHealthWidget />);
+
+    vi.waitFor(() => {
+        let healthCheckTextElement = screen.getByText("OK");
+
+        expect(healthCheckTextElement).toBeInTheDocument();
+    })
+});
+
+
+
+// test("Health check widget sees the API health route", async () => {
+// 	let renderResult = render(<ApiHealthWidget />);
+
+// 	let healthCheckTextElement = screen.getByText("ERROR");
+
+// 	expect(healthCheckTextElement).toBeInTheDocument();
+// });
