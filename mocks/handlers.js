6ccfd283b84be2e96001334afa524d96e366c27e
Delcan-f <109348946+Delcan-f@users.noreply.github.com>
2025-04-26 10:52:13 +1000

    feat: MSW implemented in dev environment

---
+++ mocks/handlers.js
@@ -0,0 +1,10 @@
+import {http, HttpResponse} from "msw";
+
+// Array of interceptors
+export const handlers = [
+	
+	http.get("http://localhost:3000/health", () => {
+		return HttpResponse.json({status: "OK - mocked!"})
+	})
+
+];
